ab3bbfc611f7f90bb6e25bf016d71ed319560143
NeginSal <neginsaljooghi@gmail.com>
2021-09-21 17:30:10 +0430

    add menu component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <div className="App">
+      <Navbar/>
 
     </div>
    );
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return <div>
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+            </nav>
+        </div>;
+    }
+}
+
+export default Navbar;
+
+
