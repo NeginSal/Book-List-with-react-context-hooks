408925a65b99591c7d02dfc591b01c4a1e466ce8
NeginSal <neginsaljooghi@gmail.com>
2021-09-24 11:06:21 +0330

    use context in navbar with context consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends React.Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const  theme = isLightTheme ? light : dark ;
-        return <div>
-            <nav style={{ background: theme.ui, color:theme.syntax }}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
             </nav>
-        </div>;
-    }
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
 
 export default Navbar;
-
-
