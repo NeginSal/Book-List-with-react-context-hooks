19aaeee7ea52e393eca3b684b54eaee4f10845e0
NeginSal <neginsaljooghi@gmail.com>
2021-09-24 10:32:23 +0330

    Accessing context in booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,13 +4,14 @@ import { ThemeContext } from '../contexts/ThemeContext';
 class BookList extends React.Component {
     static contextType = ThemeContext;
     render() {
-
+        const{ isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark ;
         return<div>
-            <div className="book-list" >
+            <div className="book-list" style={{background:theme.bg, color:theme.syntax}}>
             <ul>
-                <li>The ways of kings</li>
-                <li>The name of wind</li>
-                <li>The final empire</li>
+                <li style={{background:theme.ui, color:theme.syntax}} >The ways of kings</li>
+                <li style={{background:theme.ui, color:theme.syntax}} >The name of wind</li>
+                <li style={{background:theme.ui, color:theme.syntax}} >The final empire</li>
             </ul>
             </div>
         </div>;
