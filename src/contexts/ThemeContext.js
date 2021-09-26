41df20147ad70e67af325909de1cb478e5c9a3bf
NeginSal <neginsaljooghi@gmail.com>
2021-09-26 10:26:38 +0330

    updating context data

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,10 +8,17 @@ class ThemeContextProvider extends React.Component {
     light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
     dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
   };
+
+  toggleTheme = () => {
+    this.setState({ isLightTheme : !this.state.isLightTheme });
+  };
+
   render() {
     return (
       <div>
-        <ThemeContext.Provider value={{ ...this.state }}>
+        <ThemeContext.Provider
+          value={{ ...this.state, toggleTheme: this.toggleTheme }}
+        >
           {this.props.children}
         </ThemeContext.Provider>
       </div>
