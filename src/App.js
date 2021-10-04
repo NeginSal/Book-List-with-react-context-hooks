53154472b53306c897aa16f840a9943fcd41d542
NeginSal <neginsaljooghi@gmail.com>
2021-10-04 20:00:23 +0330

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Navbar from "./components/Navbar";
+import BookContextProvider from "./contexts/BookContext";
 
 
 const App = () => {
   return (
     <div>
+      <BookContextProvider>
+        <Navbar/>
+      </BookContextProvider>
 
     </div>
   );
