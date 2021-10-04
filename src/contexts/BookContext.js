0b7cac43ec26d29ad20d6dd1a86f2502eab7e650
NeginSal <neginsaljooghi@gmail.com>
2021-10-04 19:46:32 +0330

    add book context

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,16 +1,22 @@
-import React,{ createContext, useState } from 'react';
+import React,{createContext,useState} from 'react';
+import uuid from 'uuid/v1'
 
 export const BookContext = createContext();
+
 const BookContextProvider = (props) => {
-    const [books, Setbooks]=useState([
-        {title:'book1', id:1},
-        {title:'book2', id:2},
-        {title:'book3', id:3},
-        {title:'book4', id:4}
-    ])
+    const [books, setBooks] = useState([
+        {title: 'name of the wind', author: 'patric rothfus', id: 1 },
+        {title: 'the final empire', author: 'brandon sanderson', id: 1 }
+    ]);
+    const addBook = (title, author) => {
+        setBooks([...books, {title, author, id:uuid() }]);
+    };
+    const removeBook = (id) => {
+        setBooks(books.filter(book => book.id !== id ));
+    };
     return (
-        <BookContext.Provider value={{books}}>
-            {props.children}
+        <BookContext.Provider value={{books, addBook, removeBook}}>
+            { props.children }
         </BookContext.Provider>
      );
 }
